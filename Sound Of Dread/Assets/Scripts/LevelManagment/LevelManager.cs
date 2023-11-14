8b25b18dd34a087d6bc1995b29362d0510e33e0d
João Saraiva <32929768+Saraiv@users.noreply.github.com>
2023-11-14 15:49:01 +0000

    Fade throw levels done. Prefab created.

--- Sound Of Dread/Assets/Scripts/LevelManagment/LevelManager.cs
+++ Sound Of Dread/Assets/Scripts/LevelManagment/LevelManager.cs
@@ -5,17 +5,36 @@ public class LevelManager : MonoBehaviour{
     public enum Level{
         ForestScene,
         InsideLevel,
+        OutsideLevel,
+        Pavilion,
         Church
     }
 
     public Level levelToChange;
+    public Animator animator;
+    private static int levelToLoad;
     
     private void OnTriggerEnter(Collider collision){
         if(levelToChange == Level.ForestScene)
-            SceneManager.LoadScene(0, LoadSceneMode.Single);
+            FadeToLevel(0);
         else if(levelToChange == Level.InsideLevel)
-            SceneManager.LoadScene(1, LoadSceneMode.Single);
+            FadeToLevel(1);
+        else if(levelToChange == Level.OutsideLevel)
+            FadeToLevel(2);
+        else if(levelToChange == Level.Pavilion)
+            FadeToLevel(3);
         else if(levelToChange == Level.Church)
-            SceneManager.LoadScene(2, LoadSceneMode.Single);
+            FadeToLevel(4);
     }   
+
+    public void FadeToLevel(int levelIndex){
+        animator.SetTrigger("FadeOut");
+        levelToLoad = levelIndex;
+        Debug.Log(levelToLoad);
+    }
+
+    public void OnFadeComplete(){
+        Debug.Log(levelToLoad);
+        SceneManager.LoadScene(levelToLoad, LoadSceneMode.Single);
+    }
 }
